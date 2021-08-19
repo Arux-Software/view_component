97eb7b775277f688bcc5c3d0fe80f3f6795284d7
Hunter Braun <hunter.braun@gmail.com>
2021-08-18 23:36:47 -0500

    Remove unneeded code

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -5,8 +5,6 @@ module ViewComponent
     def render_in(view_context, &block)
       return "" unless render?
 
-      @view_context = view_context
-
       partial = self.class.name.underscore
 
       attributes = instance_variables.map do |attribute|
@@ -19,10 +17,10 @@ module ViewComponent
 
       if block_given?
         view_context.concat(
-          render(partial: "components/#{partial}", locals: { content: view_context.capture(&block) })
+          view_context.render(partial: "components/#{partial}", locals: { content: view_context.capture(&block) })
         )
       else
-        render partial: "components/#{partial}"
+        view_context.render partial: "components/#{partial}"
       end
     end
 
@@ -32,24 +30,5 @@ module ViewComponent
     def render?
       true
     end
-
-    # Re-use original view_context if we're not rendering a component.
-    #
-    # This prevents an exception when rendering a partial inside of a component that has also been rendered outside
-    # of the component. This is due to the partials compiled template method existing in the parent `view_context`,
-    # and not the component's `view_context`.
-    #
-    # @private
-    def render(options = {}, args = {}, &block)
-      if options.is_a? ViewComponent::Base
-        super
-      else
-        view_context.render(options, args, &block)
-      end
-    end
-
-    private
-
-    attr_reader :view_context
   end
 end
